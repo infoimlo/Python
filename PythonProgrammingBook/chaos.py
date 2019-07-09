5d67d1d3db898b56b458ceb840f655fd5e6c8c31
Loretta Agyemang <loretta.agyemang@gmail.com>
2019-07-09 13:27:39 -0500

    worked on individual tasks

--- PythonProgrammingBook/chaos.py
+++ PythonProgrammingBook/chaos.py
@@ -1,12 +1,22 @@
 # File: chaos.py
 # Simple program illustrating chaotic behavior.
 
+# Pseduocode
+# Program that mimiks the logistic function
+# Takes an input as x
+# manipultates input and returns a new x as output
+# performs function 10 times to see how it changes over time
+
+# Input: number between 0 & 1
+# Output: manipulation of inputed number
+# Process: x = 3.9 * x * (1 - x) performed 10 times
+
 def main():
     print("This program illustrates a chaotic function!")
 
     # Chaos theory algorithm
     # Assign input to variable x
-    x = eval(input("Enter a number between 0 and 1: "))
+    x = eval(input("Enter a number between 0 and 1: ")) # use eval to convert string input to number (not safe online)
     # Create loop that runs 10 times
     for i in range(10):
         x = 3.9 * x * (1 - x) # looks like saving changes to x to x itself?
--- PythonProgrammingBook/chap1.py
+++ PythonProgrammingBook/chap1.py
@@ -9,7 +9,7 @@ def hello():
 
 
 # Invoke function
-hello()
+#hello()
 
 
 # Design my own functions
@@ -18,7 +18,7 @@ def adder():
     b = 6
     return a + b
 
-print(adder())
+#print(adder())
 
 def paste():
     x = [1, 2, 3, 4]
@@ -29,7 +29,7 @@ def paste():
 
     print(z)
 
-paste()
+#paste()
 
 
 # Parameters/arguments - changeable parts in command
@@ -41,4 +41,52 @@ def greet(person):
 
 # Invoke function
 person = 'Loretta'
-greet(person)
+#greet(person)
+
+
+# Personal project
+# Function that displays user's age 10 years ago
+
+# Input: current age
+# Output: age 10 years ago
+# Process: subtraction of years
+
+# Pseudo code
+# Message providing instructions for how user can interact with program
+# Input: some whole number
+# Process: younger = x - 10
+# Output: Display x - 10
+
+# Define function
+def aging():
+    print("The psychic will tell you how old you were ten years ago and how old you will be 10 years into the future! ")
+    x = eval(input("Tell me your current age: "))
+    y = x - 10
+    z = x + 10
+    print("Hmmm...ten years ago, you were", y, "years old!" )
+    print("In ten years, you will be approximatly", z, "years old. You're very welcome. My gift comes from God!")
+
+# Invoke function
+#aging()
+
+# Calculator for evaluating exponential function
+# The calculator will take in a variable and spit out an evaluation for an exponential function.
+
+# Input: variable x
+# Output: manipulation of x as an addition with another variable
+# Process: Print out of calculation evaluated 10 times
+
+def calculate():
+    x = 1, 2, 3, 4, 5
+    y = 100, 90, 30, 60, 98.5
+
+    for i in range(10):
+        z = x + y
+    print(z)
+
+
+#invoke function
+calculate()
+
+# Hmmm...right now, all it does is contatenate the numbers as lists.
+# Will have to fix later! 
