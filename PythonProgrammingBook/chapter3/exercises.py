5871e36444cf52eda2ee16ff5bf6ad0086cc6396
Loretta Agyemang <loretta.agyemang@gmail.com>
2019-07-10 15:17:40 -0500

    finished part 1

---
+++ PythonProgrammingBook/chapter3/exercises.py
@@ -0,0 +1,20 @@
+# Chapter 3 - Programming Exercises
+
+import math
+
+# 1) Show result of evaluating each expression
+#print(4.0 / 10.0 + 3.5 * 2) # 7.4 - float
+#print(10 % 4 + 6/2) # 5.0 - float
+#print(abs(4 - 20 // 3) ** 3) # 8 - int
+
+
+#print(math.sqrt(4.5 - 5.0) + 7 * 3) # ValueError: math domain error?? - oh...because it's a negative value
+
+#print(math.sqrt(4.5 + 5.0) + 7 * 3) # 24.082207001484488 -
+
+#print(3 * 10 // 3 + 10 % 3) # 11 - int
+
+#print(3 ** 3) # 27 - int
+
+# Part 2 - translate mathematical expressions into Python expressions
+
